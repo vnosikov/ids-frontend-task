536023004cfd8ba144fe2edd3b1fedda44e4c3f7
Andrey Koryakovtsev <a.koryakovtsev@idynsys.org>
2022-01-30 16:39:17 +0100

    initial task

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { List } from "../../components/List";
+import { Pages } from "../../components/Pages";
+
+export const HomePage = () => {
+  return (
+    <div>
+      IDS test task
+      <List />
+      <Pages />
+    </div>
+  );
+};
